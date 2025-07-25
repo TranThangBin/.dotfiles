26ae90f6bc6e16e6925471d8fdef8dfbb7a36e76
TranThangBin <thangdev04@gmail.com>
2025-07-25 21:28:04 +0700

    NOTE: wait for hypridle and hyprpaper source syntax

--- services/hypridle.nix
+++ services/hypridle.nix
@@ -9,6 +9,7 @@
 }:
 {
   settings = {
+    "$pidofBin" = pidofBin;
     "$hyprctlBin" = hyprctlBin;
     "$loginctlBin" = "${systemd}/bin/loginctl";
     "$playerctlBin" = "${playerctl}/bin/playerctl";
@@ -16,6 +17,40 @@
     "$brightnessctlBin" = "${brightnessctl}/bin/brightnessctl";
     "$systemctlBin" = systemctlPath;
 
-    source = "${../hypr/hypridle.conf}";
+    general = {
+      lock_cmd = "$pidofBin hyprlock || $playerctlBin stop && $hyprlockBin";
+      before_sleep_cmd = "$loginctlBin lock-session";
+      after_sleep_cmd = "$hyprctlBin dispatch dpms on";
+    };
+
+    listener = [
+      {
+        timeout = 150;
+        on-timeout = "$brightnessctlBin -s set 10";
+        on-resume = "$brightnessctlBin -r";
+      }
+
+      {
+        timeout = 150;
+        on-timeout = "$brightnessctlBin -sd rgb:kbd_backlight set 0";
+        on-resume = "$brightnessctlBin -rd rgb:kbd_backlight";
+      }
+
+      {
+        timeout = 300;
+        on-timeout = "$loginctlBin lock-session";
+      }
+
+      {
+        timeout = 330;
+        on-timeout = "$hyprctlBin dispatch dpms off";
+        on-resume = "$hyprctlBin dispatch dpms on";
+      }
+
+      {
+        timeout = 1800;
+        on-timeout = "$systemctlBin suspend";
+      }
+    ];
   };
 }
